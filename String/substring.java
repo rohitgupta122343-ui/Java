3b988a0c5e43cc8735d2b767d59cd380b252cb8e
Rohit Gupta <rohitgupta122343@gmail.com>
2025-10-25 03:15:42 -0700

    Create substring.java

---
+++ String/substring.java
@@ -0,0 +1,11 @@
+package String;
+
+public class substring {
+    public static void main(String[] args) {
+         
+        String str = "here r some issue in java";
+        
+        // i want 'some'
+        System.out.println(str.substring(7,11));
+    }
+}
