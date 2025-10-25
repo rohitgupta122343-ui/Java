ce3a225214ad05efa7c4bac4990c327ca2697e26
Rohit Gupta <rohitgupta122343@gmail.com>
2025-10-25 03:08:28 -0700

    Create replace.java

---
+++ String/replace.java
@@ -0,0 +1,12 @@
+package String;
+
+public class replace {
+    
+    public static void main(String[] args){
+
+        String str = "apple";
+       String new_str  =  str.replace('a', 'b');
+
+       System.out.println(new_str);
+    }
+}
