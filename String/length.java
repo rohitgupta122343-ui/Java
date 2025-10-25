eba1fac6f66edf17eaa3c2bed249546997f2d77f
Rohit Gupta <rohitgupta122343@gmail.com>
2025-10-25 02:43:35 -0700

    Create length.java

---
+++ String/length.java
@@ -0,0 +1,12 @@
+package String;
+
+public class length {
+    public static void main(String[] args) {
+       
+        String name = "rohit";
+        String city = "mumbai";
+
+        System.out.println(name.length());
+        System.out.println(city.length());
+    }
+}
