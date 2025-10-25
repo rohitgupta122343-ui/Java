d7dac0908661a97c1eafa889234b2fbf42e4e33e
Rohit Gupta <rohitgupta122343@gmail.com>
2025-10-25 02:40:27 -0700

    Create concatenation.java

---
+++ String/concatenation.java
@@ -0,0 +1,14 @@
+
+package String;
+
+public class concatenation {
+
+    public static void main(String[] args){
+
+        String name1 = "rohit ";
+        String name2 = "Gupta";
+        String name3 = name1 + name2;
+
+        System.out.println(name3);
+    }
+}
