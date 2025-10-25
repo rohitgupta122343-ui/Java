ed8525e4ba35cd22b28fb6157dd06f94756ae5d0
Rohit Gupta <rohitgupta122343@gmail.com>
2025-10-25 02:34:50 -0700

    added

---
+++ basic/test.java
@@ -0,0 +1,12 @@
+package basic;
+class test{
+  public static void main(String[] args) {
+  
+    String name = "rohit";
+    int age = 19;
+
+    System.out.println("name "  + name);
+    System.out.println("age " + age);
+
+  }
+}
