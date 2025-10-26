88cd822aa5593f722618e2c37014d8642e063c6f
Rohit Gupta <rohitgupta122343@gmail.com>
2025-10-26 02:12:30 -0700

    Create arrayOperation.java

---
+++ Array/arrayOperation.java
@@ -0,0 +1,23 @@
+
+package Array;
+
+import java.util.Arrays;
+
+public class arrayOperation {
+
+    public static void main(String[] args) {
+        
+        int[] marks = {78,79,81,82,84};
+
+        System.out.println(Arrays.toString(marks));
+
+        // accessing of index 
+        System.out.println("index 1 : " + marks[1]);
+        System.out.println("index 3 : "+marks[3]);
+
+      // iterate For loop
+      for (int i = 0; i < marks.length; i++) {
+        System.out.println("index " + i +  " " +  marks[i]);
+      }
+    }
+}
