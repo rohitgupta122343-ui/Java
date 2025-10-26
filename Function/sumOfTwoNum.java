42626629f4f91f6de36b7122aed897db076f861e
Rohit Gupta <rohitgupta122343@gmail.com>
2025-10-26 02:21:39 -0700

    Create sumOfTwoNum.java

---
+++ Function/sumOfTwoNum.java
@@ -0,0 +1,16 @@
+package Function;
+
+public class sumOfTwoNum {
+
+    public static void sum(int a, int b) {
+        
+        int c = a+b;
+        System.out.println("sum of both number : "+c);
+    }
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        sum(a, b);
+    }
+}
