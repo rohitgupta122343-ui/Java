dce80319a8b4b6ae191f0cb6b39c42a41b220bd4
Rohit Gupta <rohitgupta122343@gmail.com>
2025-10-28 00:24:27 -0700

    Create singleLevel_Inhetitance.java

---
+++ opps/Inheritance/singleLevel_Inhetitance.java
@@ -0,0 +1,42 @@
+
+
+package opps.Inheritance;
+
+class person{
+
+    String name;
+    int age;
+    String city;
+
+}
+
+class student extends person{
+
+    int roll_no;
+
+    student(String name,int age,String city,int roll_no){
+        this.name = name;
+        this.age = age;
+        this.city = city;
+        this.roll_no = roll_no;
+    }
+
+    void get_info(){
+        System.out.println("name "+name);
+        System.out.println("age "+age);
+        System.out.println("city "+city);
+        System.out.println("roll_no "+roll_no);
+    }
+
+}
+
+public class singleLevel_Inhetitance {
+
+    public static void main(String[] args) {
+
+        student s1 = new student("rohit",19,"mumbai",413);
+
+        s1.get_info();
+        
+    }
+}
