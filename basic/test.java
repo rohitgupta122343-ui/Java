f450e77ca42abbefe2a7893293b440bedd8023a8
Rohit Gupta <rohitgupta122343@gmail.com>
2025-10-29 08:57:47 -0700

    Update test.java

--- basic/test.java
+++ basic/test.java
@@ -5,7 +5,7 @@ class test{
     String name = "rohit";
     int age = 19;
 
-    System.out.println("name "  + name);
+    System.out.println("name : "  + name);
     System.out.println("age " + age);
 
   }
