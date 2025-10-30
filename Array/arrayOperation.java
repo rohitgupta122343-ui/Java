c677f237cb7ba8277711592563546436488788e1
Rohit Gupta <rohitgupta122343@gmail.com>
2025-10-30 09:02:51 -0700

    Update arrayOperation.java

--- Array/arrayOperation.java
+++ Array/arrayOperation.java
@@ -15,7 +15,7 @@ public class arrayOperation {
         System.out.println("index 1 : " + marks[1]);
         System.out.println("index 3 : "+marks[3]);
 
-      // iterate For loop
+  
       for (int i = 0; i < marks.length; i++) {
         System.out.println("index " + i +  " " +  marks[i]);
       }
