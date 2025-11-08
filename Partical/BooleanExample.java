b0f91c8f83688efe7f940180d958d9aea7e984a0
Rohit Gupta <rohitgupta122343@gmail.com>
2025-11-08 08:00:18 -0800

    added

---
+++ Partical/BooleanExample.java
@@ -0,0 +1,26 @@
+package Partical;
+
+public class BooleanExample {
+    public static void main(String[] args) {
+
+        boolean isJava_Fun = true;
+        boolean isJava_Sad = false;
+
+        System.out.println("is Java_Fun "+isJava_Fun);
+        System.out.println("is Java_Sad "+isJava_Sad);
+
+        // Using Boolean Expression
+        int x = 10;
+        int y = 5;
+
+        System.out.println("X : "+x +" Y : " + y);
+        System.out.println("X is Greater Than Y " + (x>y));
+        System.out.println("X is Equal than Y " + (x==y));
+
+        
+        // Boolean logic
+
+        System.out.println("(x>y) && isJava_Fun "+((x>y) && isJava_Fun));
+}
+}
+
