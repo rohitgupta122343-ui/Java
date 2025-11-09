b7a41be6d6ef9bf857bb4bf064b14ffc22441d78
Rohit Gupta <rohitgupta122343@gmail.com>
2025-11-09 08:35:26 -0800

    Create PrintStringExample.java

---
+++ Partical/PrintStringExample.java
@@ -0,0 +1,12 @@
+package Partical;
+
+public class PrintStringExample {
+    
+    public static void main(String[] args) {
+         String str = "hello java";
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(str);
+        }
+    }
+}
