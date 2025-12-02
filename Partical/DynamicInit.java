7fcdd6f194aa1fc293604d0a447c32f014dc561a
Rohit Gupta <rohitgupta122343@gmail.com>
2025-12-02 08:10:51 -0800

    Create DynamicInit.java

---
+++ Partical/DynamicInit.java
@@ -0,0 +1,9 @@
+
+public class DynamicInit {
+
+    public static void main(String[] args) {
+        double r = 7.5;
+        double area =  Math.PI * r * r;
+        System.out.println(area);
+    }
+}
