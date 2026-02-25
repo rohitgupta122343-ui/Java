58a922c42a94ea9a2e5692eab75242ba2972fc43
Rohit Gupta <rohitgupta122343@gmail.com>
2026-02-25 14:11:24 -0800

    SortList

---
+++ Sem4JavaPartical/SortList.java
@@ -0,0 +1,14 @@
+
+import java.util.Arrays;
+
+class SortList{
+    public static void main(String[] args) {
+        
+        String[] list = {"bannaa","apple","orange"};
+
+        Arrays.sort(list);
+
+        System.out.print(Arrays.toString(list));
+
+    }
+}
