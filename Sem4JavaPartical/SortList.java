c7319a838dfc37cfb78d381e7f412c1ff0b2657f
Rohit Gupta <rohitgupta122343@gmail.com>
2026-03-02 10:46:07 -0800

    filler

--- Sem4JavaPartical/SortList.java
+++ Sem4JavaPartical/SortList.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 class SortList{
     public static void main(String[] args) {
         
-        String[] list = {"bannaa","apple","orange"};
+        String[] list = {"bannaa","apple","orange","mango"};
 
         Arrays.sort(list);
 
